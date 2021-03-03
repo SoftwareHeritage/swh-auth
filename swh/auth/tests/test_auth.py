bced551ed3f2465eb80877a55a9df69fa72c2e1d
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2021-03-03 12:30:16 +0100

    swh.auth: Add test for the authorization url endpoint
    Related to T3079

--- swh/auth/tests/test_auth.py
+++ swh/auth/tests/test_auth.py
@@ -3,6 +3,8 @@
 # License: GNU Affero General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
+from urllib.parse import parse_qs, urlparse
+
 import pytest
 
 from swh.auth import KeycloakOpenIDConnect
@@ -21,5 +23,25 @@ def test_auth_well_known(mock_keycloak, keycloak_open_id_connect):
     well_known_result = keycloak_open_id_connect.well_known()
     assert well_known_result is not None
     assert well_known_result == WELL_KNOWN
+
+    assert mock_keycloak.called
+
+
+def test_auth_authorization_url(mock_keycloak, keycloak_open_id_connect):
+    actual_auth_uri = keycloak_open_id_connect.authorization_url(
+        "http://redirect-uri", foo="bar"
+    )
+
+    expected_auth_url = WELL_KNOWN["authorization_endpoint"]
+    parsed_result = urlparse(actual_auth_uri)
+    assert expected_auth_url.endswith(parsed_result.path)
+
+    parsed_query = parse_qs(parsed_result.query)
+    assert parsed_query == {
+        "client_id": ["client-id"],
+        "response_type": ["code"],
+        "redirect_uri": ["http://redirect-uri"],
+        "foo": ["bar"],
+    }
 
     assert mock_keycloak.called
