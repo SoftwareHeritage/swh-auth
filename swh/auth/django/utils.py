679c0a5e608a0bb7833c61b1e658a0507d9c32bc
Antoine Lambert <antoine.lambert@inria.fr>
2021-03-17 18:02:03 +0100

    django/utils: Get access token renewal date from proper dict field
    In the dictionary decoded from an OIDC access token, the iat field store
    the token creation date while the auth_time field store the date the
    OIDC session was opened.
    
    In order to get an accurate authentication time, the date stored in
    the iat field must be used as it corresponds to the time an access
    token was refreshed and thus the latest valid authentication date.

--- swh/auth/django/utils.py
+++ swh/auth/django/utils.py
@@ -80,8 +80,7 @@ def oidc_user_from_profile(
     user = oidc_user_from_decoded_token(decoded_token, client_id=oidc_client.client_id)
 
     # get authentication init datetime
-    auth_time = decoded_token.get("auth_time", decoded_token["iat"])
-    auth_datetime = datetime.fromtimestamp(auth_time)
+    auth_datetime = datetime.fromtimestamp(decoded_token["iat"])
     exp_datetime = datetime.fromtimestamp(decoded_token["exp"])
 
     # compute OIDC tokens expiration date
